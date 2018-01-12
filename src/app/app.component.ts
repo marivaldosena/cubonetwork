0d7f8d6df20b291cda2f066ea711f03930d8294b
Marivaldo Sena <marivaldo.sena@live.com>
2018-01-12 21:40:05 -0200

    Inclusão de gráfico Chart.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import { ChartComponent } from 'angular2-chartjs';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CADASTRE-SE';
+  chart_data = {};
+  chart_options = {};
+  chart_type = '';
+  total_share = 0;
 
   constructor() {}
+
+  ngOnInit() {
+    this.total_share = this.getTotalShare();
+    this.enableChart();
+  }
 
   /*
    * Refatorar: Nas próximas iterações, transferir o
@@ -46,4 +57,36 @@ export class AppComponent {
       share: 40
     }
   ];
+
+
+  enableChart() {
+    let rotulos = this.employees.map(e => e.name + ' ' + e.lastname);
+    this.chart_type = 'doughnut';
+    this.chart_data = {
+      labels: rotulos,
+      datasets: [
+        {
+          data: this.employees.map(e => e.share),
+          backgroundColor: [
+            '#F44336', '#9C27B0', '#673AB7', '#FF9800',
+            '#E91E63', '#03A9F4', '#00BCD4', '#795548',
+            '#009688', '#4CAF50', '#8BC34A', '#9E9E9E',
+            '#673AB7', '#3F51B5', '#2196F3', '#FF5722',
+            '#CDDC39', '#FFEB3B', '#FFC107', '#607D8B'
+          ]
+        }
+      ]
+    };
+    this.chart_options = {
+      responsive: true,
+      legend: { position: 'right'},
+      animation: {
+        animateScale: true
+      }
+    };
+  }
+
+  getTotalShare() {
+    return this.employees.reduce((total, e) => total + e.share, 0);
+  }
 }
