c65b82aaa118da3c2281d76eeb3437f83e3f11a2
Marivaldo Sena <marivaldo.sena@live.com>
2018-01-13 14:58:15 -0200

    Configuração de pré-processador SASS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css.scss']
 })
 export class AppComponent implements OnInit {
   title = 'CADASTRE-SE';
